e64b4dbe422e9c4da0c2514c56809aea0eedad64
AlexisNarvaez <alexisnarvaez97@hotmail.com>
2020-04-13 16:44:43 -0500

    WebPack Dev

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -3,7 +3,7 @@ export const saludar = ( nombre ) => {
     console.log('Creando etiqueta H1');
 
     const h1 = document.createElement('h1');
-    h1.innerText = `Hola, ${nombre}`;
+    h1.innerText = `Que tal, ${nombre}`;
 
     document.body.append(h1);
 
